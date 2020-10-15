efe8ddc82ebbd64bf2652f3509e27479cdf13aa9
Brian Phillips <brian.phillips201@gmail.com>
2020-10-15 15:52:27 -0700

    deleted postLayout component

--- src/components/postLayout/postLayout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import * as styles from "./postLayout.module.scss"
-
-const PostLayout: React.FC = ({ children }) => {
-  return <article className={styles.article}>{children}</article>
-}
-
-export default PostLayout
