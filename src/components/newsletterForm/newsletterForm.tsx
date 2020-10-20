0f375076fc5147ab834ae8aa705cc9ccb1a83e14
Brian Phillips <brian.phillips201@gmail.com>
2020-10-19 18:21:35 -0700

    styled the newsletter form

--- src/components/newsletterForm/newsletterForm.tsx
+++ src/components/newsletterForm/newsletterForm.tsx
@@ -12,18 +12,21 @@ const NewsletterForm: React.FC<FormHTMLAttributes<HTMLElement>> = () => {
             </Cell>
             <Cell middle width={1}>
                 <form action="https://xyz.us15.list-manage.com/subscribe/post?u=03287c1370c469c08fd6ae8c2&amp;id=c43332b02e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
-                    <div id="mc_embed_signup_scroll">
-                    
-                    <div className="mc-field-group">
-                        <label htmlFor="mce-EMAIL">Email Address</label>
-                        <input type="email" value="" name="EMAIL" className="required email" id="mce-EMAIL" />
-                    </div>
-                    <div id="mce-responses" className="clear">
-                        <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
-                        <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
-                    </div>
-                    <div style={{ position: "absolute", left: "-5000px"}} aria-hidden="true"><input type="text" name="b_03287c1370c469c08fd6ae8c2_c43332b02e" tabIndex={-1} value="" /></div>
-                    <div className="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" /></div>
+                    <div id="mc_embed_signup_scroll">                
+                        <div className="mc-field-group">
+                            <label htmlFor="mce-EMAIL">Email Address</label>
+                            <input type="email" defaultValue="" name="EMAIL" placeholder="Enter your email..." className="required email" id="mce-EMAIL" />
+                        </div>
+                        <div id="mce-responses" className="clear">
+                            <div className="response" id="mce-error-response" style={{ display: "none" }}></div>
+                            <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
+                        </div>
+                        <div style={{ position: "absolute", left: "-5000px"}} aria-hidden="true">
+                            <input type="text" name="b_03287c1370c469c08fd6ae8c2_c43332b02e" tabIndex={-1} defaultValue="" />
+                        </div>
+                        <div className="clear">
+                            <button type="submit" name="subscribe" id="mc-embedded-subscribe">{language.newsletter.submitButton}</button>
+                        </div>
                     </div>
                 </form>
             </Cell>
