54d2fcf5606cba8c9d5198b23e9f8c89bdf4166d
Brian Phillips <brian.phillips201@gmail.com>
2020-10-21 23:23:10 -0700

    simple styling for 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import Layout from '../components/layout/layout'
 import SEO from '../components/seo'
+import Wrapper from '../components/wrapper/wrapper'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Page not found" />
+    <Wrapper>
+      <h1>404</h1>
+      <p>That page doesn't exist, sadly.</p>
+    </Wrapper>
   </Layout>
 )
 
