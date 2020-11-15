03ab12cbe9db804da9f14457d78c390e6a8aee38
Brian Phillips <brian.phillips201@gmail.com>
2020-11-14 18:11:02 -0800

    fixed broken redirect and added 404 home redirect

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,6 @@
-import React from 'react'
+import { navigate } from 'gatsby'
 
-import Layout from '../components/layout/layout'
-import SEO from '../components/seo'
-import Wrapper from '../components/wrapper/wrapper'
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="Page not found" />
-    <Wrapper>
-      <h1>404</h1>
-      <p>That page doesn't exist, sadly.</p>
-    </Wrapper>
-  </Layout>
-)
-
-export default NotFoundPage
+export default function NotFoundPage() {
+  navigate('/', { replace: true })
+  return null
+}
