6670aba776b0dfa10e91eaecfb3af4e308aafa07
Brian Phillips <brian.phillips201@gmail.com>
2020-11-24 13:45:17 -0800

    updated my About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,6 +6,8 @@ import Circles from '../components/Circles/Circles'
 import ActionLink from '../components/ActionLink/ActionLink'
 import * as styles from '../components/Layout/Layout.module.scss'
 import language from '../utils/language'
+import StateMap from '../components/StateMap/StateMap'
+import { Link } from 'gatsby'
 
 const About: React.FC = () => (
   <Layout>
@@ -14,37 +16,85 @@ const About: React.FC = () => (
       <Circles />
     </Wrapper>
 
-    <Wrapper width="thin">
+    <Wrapper width="thin" as="article">
       <h1>Hello and welcome.</h1>
 
+      <p>I’m Brian Phillips, a front end engineer, writer, and investor.</p>
+
+      <h2>I’ve lived in some fun places.</h2>
+
       <p>
-        I'm Brian Phillips, a software engineer, writer, and investor living in
-        California. I grew up in a small southern town and left home at 25 to
-        work at cool companies with cool people. In recent years, I was an
-        engineering manager leading a team of 17 engineers.
+        At twenty-five, I moved with my wife to Memphis, Tennessee, then later
+        to San Luis Obispo, California.
+      </p>
+
+      <p>(In 2021, we’ll call NYC our home.)</p>
+
+      <StateMap />
+
+      <h2>I’ve worked with some cool people.</h2>
+
+      <p>
+        I’ve been a front end engineer for many years and recently spent four
+        years leading a software engineering team at a global wellness company.
+        Before that, I designed and built websites in the agency and freelance
+        world for a few years, working with some splendid clients.
       </p>
 
       <p>
-        In August 2020, I took a career break, during which I've been spending
-        time with my wife, building web apps, writing stories, and playing video
-        games.
+        In August 2020,{' '}
+        <Link to="/blog/i-quit-my-job-with-no-plan-should-you-do-the-same">
+          I quit my job to take a career break
+        </Link>
+        . Since then, I‘ve{' '}
+        <a
+          href="https://github.com/bphillips201"
+          target="_blank"
+          rel="noopener nofollow noreferrer"
+        >
+          coded
+        </a>{' '}
+        daily, <Link to="/blog">written</Link> frequently, and ended most days
+        with hikes and video games.
       </p>
 
-      <ul className={styles.list}>
-        <li>
-          <ActionLink
-            to={language.about.newsletter.url}
-            text={language.about.newsletter.text}
-          />
-        </li>
+      <p>
+        <ActionLink
+          to={language.about.linkedIn.url}
+          text={language.about.linkedIn.text}
+        />
+      </p>
 
-        <li>
-          <ActionLink
-            to={language.about.linkedIn.url}
-            text={language.about.linkedIn.text}
-          />
-        </li>
-      </ul>
+      <h2>I have a blog you might enjoy.</h2>
+
+      <p>
+        I’ve been a writer for many years. Recently, I’ve written things like a{' '}
+        <Link to="/blog/31-lessons-learned-in-31-years">
+          list of 31 life lessons learned in 31 years
+        </Link>
+        ,{' '}
+        <Link to="/blog/the-multi-level-marketer-and-i">
+          a tale of multi-level marketing indoctrination
+        </Link>
+        , and some{' '}
+        <Link to="/blog/i-will-now-gush-about-words">
+          cheeky thoughts on the joy of writing
+        </Link>
+        .
+      </p>
+
+      <p>
+        I also send out a semi-infrequent newsletter with my latest posts, as
+        well as the occasional list of cool books, video games, and more. It’s a
+        fun time for geeks like me.
+      </p>
+
+      <p>
+        <ActionLink
+          to={language.about.newsletter.url}
+          text={language.about.newsletter.text}
+        />
+      </p>
     </Wrapper>
   </Layout>
 )
