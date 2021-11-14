60bed14ef8c50a67635f9bd8544ad4b7d5a54e44
Brian Phillips <brian.phillips201@gmail.com>
2021-11-14 13:04:01 -0500

    Change header wrapper

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
   const Component: any = isHome ? 'h1' : 'div'
 
   return (
-    <Wrapper as="header" color="white" className="flex items-center">
+    <header className="flex items-center mx-auto max-w-7xl p-6">
       <Component className="flex-1 p-0 m-0 leading-none max-h-8">
         <Logo animate={isHome} />
       </Component>
@@ -18,7 +18,8 @@ const Header: React.FC = () => {
         <ul className="p-0 m-0">
           <li className="inline-block m-0">
             <Link
-              className="no-underline pb-2 pt-2 ml-8 font-bold font-header transition-colors hover:text-blue-600"
+              className="
+                no-underline pb-2 pt-2 ml-8 font-bold font-header transition-colors hover:text-blue-600"
               activeClassName="text-blue-600"
               to="/blog"
             >
@@ -36,7 +37,7 @@ const Header: React.FC = () => {
           </li>
         </ul>
       </nav>
-    </Wrapper>
+    </header>
   )
 }
 
