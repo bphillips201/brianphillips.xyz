0b681bd214a76499c8af4e055b2f94106f7fb54e
Brian Phillips <brian.phillips201@gmail.com>
2021-11-14 12:41:20 -0500

    Update header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,27 +3,34 @@ import Logo from '../Logo/Logo'
 import { Link } from 'gatsby'
 import Wrapper from '../Wrapper/Wrapper'
 import { useLocation } from '@reach/router'
-import * as styles from './Header.module.scss'
 
 const Header: React.FC = () => {
   const isHome = useLocation().pathname === '/'
   const Component: any = isHome ? 'h1' : 'div'
 
   return (
-    <Wrapper as="header" color="white" className={styles.header}>
-      <Component className={styles.logo}>
+    <Wrapper as="header" color="white" className="flex items-center">
+      <Component className="flex-1 p-0 m-0 leading-none max-h-8">
         <Logo animate={isHome} />
       </Component>
 
-      <nav aria-label="navigation" className={styles.nav}>
-        <ul>
-          <li>
-            <Link activeClassName={styles.active} to="/blog">
+      <nav aria-label="navigation" className="flex-1 text-right">
+        <ul className="p-0 m-0">
+          <li className="inline-block m-0">
+            <Link
+              className="no-underline pb-2 pt-2 ml-8 font-bold font-header transition-colors hover:text-blue-600"
+              activeClassName="text-blue-600"
+              to="/blog"
+            >
               Blog
             </Link>
           </li>
-          <li>
-            <Link activeClassName={styles.active} to="/about">
+          <li className="inline-block">
+            <Link
+              className="no-underline pb-2 pt-2 ml-8 font-bold font-header transition-colors hover:text-blue-600"
+              activeClassName="text-blue-600"
+              to="/about"
+            >
               About
             </Link>
           </li>
