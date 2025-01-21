baf41f5f1d565b28c99d98123e1c92c766a45408
Brian Phillips <brian.phillips201@gmail.com>
2025-01-21 16:10:16 -0500

    Remove description

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
   siteMetadata: {
     title: `Brian Phillips`,
-    description: `a writer, investor, and programmer`,
+    description: ``,
     author: `Brian Phillips`,
     siteUrl:
       process.env.NODE_ENV === 'development'
